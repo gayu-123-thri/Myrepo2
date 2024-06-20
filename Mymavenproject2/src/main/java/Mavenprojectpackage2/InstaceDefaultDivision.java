9002756e5df8780ca5cc1dde60e527a7aa649ddc
gayut <gayut@192.168.1.5>
2024-06-20 23:37:08 +0530

    commit1

---
+++ Mymavenproject2/src/main/java/Mavenprojectpackage2/InstaceDefaultDivision.java
@@ -0,0 +1,18 @@
+package Mavenprojectpackage2;
+
+public class InstaceDefaultDivision 
+{
+	int a=10;
+	int b=2;
+	public void div() 
+	{
+		int c=a/b;
+		System.out.println(c);
+	}
+	public static void main(String[] args) 
+	{
+		InstaceDefaultDivision obj=new InstaceDefaultDivision();
+		obj.div();
+	}
+
+}
